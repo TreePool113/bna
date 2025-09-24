adf7cad4aefee70096cf2e81aa4ed739810f39b8
TreePool113 <phuclinh777@gmail.com>
2025-09-25 01:18:41 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,6 @@
 import type { NextConfig } from "next";
 
+
 const nextConfig: NextConfig = {
   // output: "export",   // bắt buộc để export ra static HTML
   // images: {
--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,11 +1,10 @@
-import Navbar from "@/components/navbar";
-import { Links } from "@/components/navlinks";
+
 export default function Courses() {
     return (
         <div>
             <div className="max-w-7xl mx-auto p-4">
                 <h1 className="text-3xl font-bold mb-4">Our Courses</h1>
-                <p className="mb-2">Explore our wide range of basketball courses designed to help you improve your skills on the court. Whether you're a beginner or looking to enhance your techniques, we have a program suitable for every player.</p>
+                <p className="mb-2">Explore our wide range of basketball courses designed to help you improve your skills on the court. Whether you are a beginner or looking to enhance your techniques, we have a program suitable for every player.</p>
                 <ul className="list-disc list-inside mb-4">
                     <li className="mb-1">Course 1: Basketball Basics</li>
                     <li className="mb-1">Course 2: Shooting Techniques</li>
